ad0278057ee52088bb60ccda145d2ff1578fe679
Acer <Acer@DESKTOP-4LGIQ1P>
2022-11-05 10:06:27 +0200

    Add project files.

---
+++ Data/Sirb_Maria_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sirb_Maria_Lab2.Models;
+
+namespace Sirb_Maria_Lab2.Data
+{
+    public class Sirb_Maria_Lab2Context : DbContext
+    {
+        public Sirb_Maria_Lab2Context (DbContextOptions<Sirb_Maria_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sirb_Maria_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Sirb_Maria_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
