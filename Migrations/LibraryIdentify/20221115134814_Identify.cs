c9442a5d7f093a0fadb2f54cae1a278664f614f7
Sirb Maria <sirbmaria27@gmail.com>
2022-11-15 15:56:07 +0200

    LAB5TEMA

---
+++ Migrations/LibraryIdentify/20221115134814_Identify.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sirb_Maria_Lab2.Migrations.LibraryIdentify
+{
+    public partial class Identify : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
