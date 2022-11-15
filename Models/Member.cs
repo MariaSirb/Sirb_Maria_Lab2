0f2d210746f727e99c8511b600d30a0b70ae3252
Sirb Maria <sirbmaria27@gmail.com>
2022-11-15 18:15:30 +0200

    Lab6tema

--- Models/Member.cs
+++ Models/Member.cs
@@ -17,7 +17,6 @@ namespace Sirb_Maria_Lab2.Models
         public string? Adress { get; set; }
         public string Email { get; set; }
 
-       // [RegularExpression(@"^\(?([0-9]{4})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})$",
             //ErrorMessage = "Telefonul trebuie sa fie de forma '0722-123-123' sau '0722.123.123' sau '0722 123 123'")]
         [RegularExpression(@"^([0]{1})[-. ]?([0-9]{3})?[-. ]?([0-9]{3})[-. ]?([0-9]{3})$",
             ErrorMessage = "Telefonul trebuie sa inceapa neaparatcu cifra 0'")]
